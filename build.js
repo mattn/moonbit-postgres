a8e70e0f7caf9f142579063d054def93c3e0e02e
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2026-01-11 19:56:12 +0900

    update build.js

--- build.js
+++ build.js
@@ -1,4 +1,16 @@
 #!/usr/bin/env node
-// Prebuild script for mattn/postgres
-console.log(JSON.stringify({}));
+
+// Pre-build config script for postgres module
+// This script outputs link configuration that will be propagated to dependents
+
+const config = {
+  link_configs: [
+    {
+      package: "mattn/postgres",
+      link_libs: ["pq"]
+    }
+  ]
+};
+
+console.log(JSON.stringify(config));
 
